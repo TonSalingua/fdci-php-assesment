b1514343b2b95f940019fc92e648d41557f052c3
ton-salingua <salingua.joseanthony@gmail.com>
2023-06-02 16:35:00 +0800

    first commit

---
+++ app/Http/Controllers/CustomAuthController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+
+
+class CustomAuthController extends Controller
+{
+    public function login()
+    {
+        return view("auth.login");
+    }
+
+    public function registration()
+    {
+        return view("auth.registration");
+    }
+
+    public function registerUser(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:5|max:10'
+        ]);
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $res = $user->save();
+        if ($res) {
+            return back()->with('success', 'Registered Succesfully');
+        } else {
+            return back()->with('failed', 'Please Register Again');
+        }
+    }
+
+    public function loginUser(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:5|max:10'
+        ]);
+        $user = User::where('email', '=', $request->email)->first();
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                $request->session()->put('loginID', $user->id);
+                return redirect('dashboard');
+            } else {
+                return back()->with('fail, login again');
+            }
+        } else {
+            return back()->with('failed', 'Please Login Again');
+        }
+    }
+
+    public function dashboard()
+    {
+        $data = array();
+        if (Session::has('loginID')) {
+            $data = User::where('id', '=', Session::get('loginID'))->first();
+        }
+        return view('dashboard', compact('data'));
+    }
+
+
+    public function logout()
+    {
+        if (Session::has('loginID')) {
+            Session::pull('loginID');
+            return redirect('login');
+        }
+    }
+}
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Custom Auth Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body>
+   <div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4" style="margin-top:20px">
+            <h4>Login</h4>
+            <hr>
+            <form action="{{{route('login-user')}}}" method="post">
+                @if(Session::has('success'))
+                <div class="alert alert-success">{{Session::get('success')}}</div>
+                @endif
+                @if(Session::has('failed'))
+                <div class="alert alert-danger">{{Session::get('failed')}}</div>
+                @endif
+                @csrf
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" class="form-control" placeholder="Enter E-mail" name="email"  value="{{old('email')}}">
+                <span class="text-danger">@error('email'){{$message}}@enderror </span>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" placeholder="Enter Password" name="password"  value="{{old('password')}}">
+                <span class="text-danger">@error('password'){{$message}}@enderror </span>
+            </div>
+            <div class="for group">
+                <button class="btn btn-block btn-primary" type="submit">Login</button>
+            </div>
+            <br>
+           <a href="registration">New User? Register Here</a>
+            </form>
+        </div>
+    </div>
+   </div>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
+</html>
