0780297bc42b29def9cd045a0bbb472fce583828
ton-salingua <salingua.joseanthony@gmail.com>
2023-06-03 10:38:14 +0800

    commit routes change

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\CustomAuthController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/login', [CustomAuthController::class, 'login']);
